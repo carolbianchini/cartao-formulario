30c3d4255b035e289ed26ebe6dadab8444e767ad
carrottown <anabianchini@outlook.com.br>
2023-04-27 15:45:22 -0300

    adicionando validação de dados

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,29 +1,108 @@
-document.getElementById('numero').oninput = () =>{
-    document.querySelector('.num-cartao').innerText = document.getElementById('numero').value;
+var inputNumero = document.getElementById("numero");
+var inputNome = document.getElementById('nome-titular');
+var inputCvv = document.getElementById('cvv')
+var erroNumero = document.getElementById("erro-numero");
+var erroNome = document.getElementById("erro-nome");
+var valorAnteriorNome = inputNome.value;
+var valorAnteriorNumero = inputNumero.value;
+var valorAnteriorCvv = inputCvv.value;
+
+
+
+//Adicionando o texto ao cartão
+
+inputNumero.onkeydown = () =>{
+    document.querySelector('.num-cartao').innerText = inputNumero.value;
 }
 
-document.getElementById('nome-titular').oninput = () =>{
+document.getElementById('nome-titular').onkeydown = () =>{
     document.querySelector('.nome-cartao').innerText = document.getElementById('nome-titular').value;
 }
 
-var filtroTeclas = function(event) {
-  return ((event.charCode >= 48 && event.charCode <= 57) || (event.keyCode == 45 || event.charCode == 46))
-}
 
-//*tirar número
+//Tirar letras e caracteres especiais
 
-var inputNome = document.querySelector("#nome-titular");
+//Nome
+
 inputNome.addEventListener("keypress", function(e) {
-    var keyCode = (e.keyCode ? e.keyCode : e.which);
-  
-  if (keyCode > 47 && keyCode < 58) {
-    e.preventDefault();
+  var valor = this.value;
+    if(!checkChar(e)) {
+      erroNome.style.display = "block";
+      e.preventDefault();
+      
+  }else {
+    erroNome.style.display = "none";
+    valorAnteriorNome = valor;
   }
 });
 
-document.querySelector("#cvv") // seleciona o campo pelo atributo "name"
-.addEventListener("input", function(){ // evento "input" que detecta mudança de valor no campo
-   var cars = this.value.length; //conta a quantidade de caracteres
-   // se for menor ou igual a 50, altera texto com o valor no span
-   if(cars <= 5) document.querySelector(".limite").textContent = cars;
+function checkChar(e) {
+    var char = String.fromCharCode(e.keyCode);
+    var pattern = '[a-zA-Z ]';
+    if (char.match(pattern)) {
+      return true;
+  }
+}
+
+
+//numero
+
+inputNumero.addEventListener("keypress", function(evento) {
+  var valor = this.value;
+    if(!checkCharNumber(evento)) {
+      erroNumero.style.display = "block";
+      evento.preventDefault();
+      
+  }else {
+    erroNumero.style.display = "none";
+    valorAnteriorNumero = valor;
+  }
 });
+
+function checkCharNumber(evento) {
+    var char = String.fromCharCode(evento.keyCode);
+    var patternNumber = '[0-9 ]';
+    if (char.match(patternNumber)) {
+      return true;
+  }
+}
+
+//cvv
+
+inputCvv.addEventListener("keypress", function(evt) {
+  var valor = this.value;
+    if(!checkCharCvv(evt)) {
+      erroNumero.style.display = "block";
+      evt.preventDefault();
+      
+  }else {
+    erroNumero.style.display = "none";
+    valorAnteriorCvv = valor;
+  }
+});
+
+function checkCharCvv(evt) {
+    var char = String.fromCharCode(evt.keyCode);
+    var patterCvv = '[0-9]';
+    if (char.match(patterCvv)) {
+      return true;
+  }
+}
+
+
+
+//Máscara Cartão 
+
+inputNumero.addEventListener('keypress', () => {
+    let inputLength = inputNumero.value.length
+
+    if (inputLength == 4 || inputLength == 9 || inputLength == 14) {
+        inputNumero.value += ' '
+    }
+
+  })
+
+
+
+
+
