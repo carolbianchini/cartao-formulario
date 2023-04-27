508e84c28947680f8d75c78f9d485db15b2625a7
carrottown <anabianchini@outlook.com.br>
2023-04-26 21:47:02 -0300

    adicionando validação

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -5,3 +5,25 @@ document.getElementById('numero').oninput = () =>{
 document.getElementById('nome-titular').oninput = () =>{
     document.querySelector('.nome-cartao').innerText = document.getElementById('nome-titular').value;
 }
+
+var filtroTeclas = function(event) {
+  return ((event.charCode >= 48 && event.charCode <= 57) || (event.keyCode == 45 || event.charCode == 46))
+}
+
+//*tirar número
+
+var inputNome = document.querySelector("#nome-titular");
+inputNome.addEventListener("keypress", function(e) {
+    var keyCode = (e.keyCode ? e.keyCode : e.which);
+  
+  if (keyCode > 47 && keyCode < 58) {
+    e.preventDefault();
+  }
+});
+
+document.querySelector("#cvv") // seleciona o campo pelo atributo "name"
+.addEventListener("input", function(){ // evento "input" que detecta mudança de valor no campo
+   var cars = this.value.length; //conta a quantidade de caracteres
+   // se for menor ou igual a 50, altera texto com o valor no span
+   if(cars <= 5) document.querySelector(".limite").textContent = cars;
+});
